ffbf6f7f2c7549ab3c953facc2134120fd698697
vikas.v6543@gmail.com <vikas.v6543@gmail.com>
2023-08-29 22:06:21 -0700

    First Commit

---
+++ calc.js
@@ -0,0 +1,14 @@
+function sum(num1, num2) {
+    return num1-num2;
+}
+console.log("Sub Of Two Number is =>  ",sum(3,4));
+
+function sum(num1, num2) {
+    return num1+num2;
+}
+console.log("Sum Of Two Number is =>  ",sum(3,4));
+
+function sum(num1, num2) {
+    return num1*num2;
+}
+console.log("Mul Of Two Number is =>  ",sum(3,4));

00d1d76cf25b7d2036f1940a7ca3310147407781
vikas.v6543@gmail.com <vikas.v6543@gmail.com>
2023-08-29 23:35:33 -0700

    added division method

--- calc.js
+++ calc.js
@@ -1,14 +1,20 @@
-function sum(num1, num2) {
+console.log("Calculator Example");
+function sub(num1, num2) {
     return num1-num2;
 }
-console.log("Sub Of Two Number is =>  ",sum(3,4));
+console.log("Sub Of Two Number is =>  ",sub(3,4));
 
 function sum(num1, num2) {
     return num1+num2;
 }
 console.log("Sum Of Two Number is =>  ",sum(3,4));
 
-function sum(num1, num2) {
+function mul(num1, num2) {
     return num1*num2;
 }
-console.log("Mul Of Two Number is =>  ",sum(3,4));
+console.log("Mul Of Two Number is =>  ",mul(3,4));
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+console.log("divide Of Two Number is =>  ",sum(3,4));
